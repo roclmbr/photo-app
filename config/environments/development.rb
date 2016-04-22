62c681d157f86489e64a49d7665ca3e03403b157
Steve Adamson <mail@steveadamson.com>
2016-04-22 12:48:37 +0000

    Added email verification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,14 +8,16 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
+  
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'https://photo-app-roclmbr.c9users.io/users/sign_up', port: 3000 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
